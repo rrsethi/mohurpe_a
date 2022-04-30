c5e7086cbcd2ebacfe3b9a83093b4c01a58b1668
rrsethi <rrs.niit@gmail.com>
2022-05-01 00:37:26 +0530

    hsjkrf

---
+++ lib/screens/receiverDetail/temp.dart
@@ -0,0 +1,145 @@
+
+// import 'package:mohurpe_a/screens/login/components/body.dart';
+// class LastFiveUserID {
+//   late String userID;
+
+//   LastFiveUserID({
+//     required this.userID,
+//   });
+// }
+
+// class ReceiverUserIDScreen extends StatelessWidget {
+//   const ReceiverUserIDScreen({Key? key}) : super(key: key);
+//   @override
+//   State<ReceiverUserIDScreen> createState() => _ReceiverUserIDScreenState();
+// }
+
+// class _ReceiverUserIDScreenState extends State<ReceiverUserIDScreen> {
+//   List<LastFiveUserID> userid = [
+//     LastFiveUserID(userID: "rrsethi"),
+//     LastFiveUserID(userID: "rrsethi"),
+//     LastFiveUserID(userID: "rrsethi"),
+//     LastFiveUserID(userID: "rrsethi"),
+//     LastFiveUserID(userID: "rrsethi"),
+//   ];
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size = MediaQuery.of(context).size;
+//     return Scaffold(
+//         body: AnnotatedRegion<SystemUiOverlayStyle>(
+//             value: SystemUiOverlayStyle.light
+//                 .copyWith(statusBarColor: Theme.of(context).primaryColor),
+//             child: SafeArea(
+//                 child: SizedBox(
+//               height: size.height,
+//               width: size.width,
+//               child: SingleChildScrollView(
+//                 padding: EdgeInsets.fromLTRB(0, size.height * 0.1, 0, 0),
+//                 child: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     crossAxisAlignment: CrossAxisAlignment.center,
+//                     children: <Widget>[
+//                       Positioned(
+//                         top: size.height * 0.2,
+//                         child: Image.asset(
+//                           "assets/images/logo.png",
+//                           width: size.width * 0.6,
+//                         ),
+//                       ),
+//                       Padding(
+//                         padding:
+//                             EdgeInsets.fromLTRB(0, size.height * 0.01, 0, 0),
+//                         child: Text(
+//                           "MohurPe",
+//                           textAlign: TextAlign.center,
+//                           style: TextStyle(
+//                               fontWeight: FontWeight.bold,
+//                               fontSize: size.height * 0.03),
+//                         ),
+//                       ),
+//                       // const Text(
+//                       //       'Name',
+//                       //       textAlign: TextAlign.left,
+//                       // ),
+//                       // const Text(
+//                       //   "Enter phone number:",
+//                       //   textAlign: TextAlign.left,
+//                       // ),
+//                       Padding(
+//                         padding: EdgeInsets.fromLTRB(
+//                             size.width * 0.05,
+//                             size.height * 0.1,
+//                             size.width * 0.05,
+//                             size.height * 0.01),
+//                         child: TextField(
+//                           decoration: InputDecoration(
+//                             labelText: 'User ID',
+//                             enabledBorder: OutlineInputBorder(
+//                               borderSide: const BorderSide(
+//                                   width: 3, color: Colors.grey),
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                             focusedBorder: OutlineInputBorder(
+//                               borderSide: const BorderSide(
+//                                   width: 3, color: kPrimaryColor),
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                             hintText: "Enter receiver's user id",
+//                           ),
+//                         ),
+//                       ),
+
+//                       Padding(
+//                         padding: EdgeInsets.fromLTRB(size.width * 0.05, 15,
+//                             size.width * 0.05, size.height * 0.01),
+//                         child: TextField(
+//                           decoration: InputDecoration(
+//                             labelText: 'Amount',
+//                             enabledBorder: OutlineInputBorder(
+//                               borderSide: const BorderSide(
+//                                   width: 3, color: Colors.grey),
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                             focusedBorder: OutlineInputBorder(
+//                               borderSide: const BorderSide(
+//                                   width: 3, color: kPrimaryColor),
+//                               borderRadius: BorderRadius.circular(15),
+//                             ),
+//                             hintText: "Enter Amount",
+//                           ),
+//                         ),
+//                       ),
+
+//                       Padding(
+//                         padding:
+//                             EdgeInsets.fromLTRB(0, size.height * 0.05, 0, 0),
+//                         child: ElevatedButton(
+//                           style: ElevatedButton.styleFrom(
+//                             minimumSize: Size(size.width * 0.7, 55),
+//                             shape: RoundedRectangleBorder(
+//                                 borderRadius: BorderRadius.circular(20)),
+//                           ),
+//                           child: const Text(
+//                             'PAY',
+//                             style: TextStyle(
+//                               fontSize: 20.0,
+//                               color: Colors.white,
+//                             ),
+//                           ),
+//                           onPressed: () {
+//                             Navigator.push(
+//                               context,
+//                               MaterialPageRoute(
+//                                 builder: (context) {
+//                                   return const HomeScreen();
+//                                 },
+//                               ),
+//                             );
+//                           },
+//                         ),
+//                       )
+//                     ]),
+//               ),
+//             ))));
+//   }
+// }
